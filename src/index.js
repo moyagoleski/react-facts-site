c334f2c56c9a011288712c90e4ca0120cfe979de
moyagoleski <moyagoleski@gmail.com>
2022-01-17 18:10:54 -0500

    Update to index.js
    - separate components added including a Header, MainContent, Footer, and Page

--- src/index.js
+++ src/index.js
@@ -7,18 +7,50 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 
-const page = (
-  <div>
-    <img src="logo512.png" width="40px"/>
-    <h1>Fun Facts about React</h1>
-    <ul>
-      <li>Was first released in 2013</li>
-      <li>Was originally created by Jordan Walke</li>
-      <li>Has well over 100k stars on Github</li>
-      <li>Is maintained by Facebook</li>
-      <li>Powers thousands of enterprise apps, including mobile apps</li>
-    </ul>
-  </div>
-)
+function Header() {
+  return (
+    <div>
+      <header>
+        <nav>
+          <img src="logo512.png" width="40px" alt="react logo"/>
+        </nav>
+      </header>
+    </div>
+  )
+}
 
-ReactDOM.render(page, document.getElementById("root"))
+function MainContent() {
+  return (
+    <div>
+      <h1>Fun Facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100k stars on Github</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <footer>
+      <small>© 2022 Goleski development. All rights reserved.</small>
+    </footer>
+  )
+}
+
+function Page() {
+  return (
+    <div>
+      <Header />
+      <MainContent />
+      <Footer />
+    </div>
+  )
+
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"))
