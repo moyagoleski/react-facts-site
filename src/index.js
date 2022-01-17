21ff0a99896a9e04f55bc5d772ab4da410728eb5
moyagoleski <moyagoleski@gmail.com>
2022-01-16 21:36:25 -0500

    Updates to index.html, index.js
    - cleaned up code in index.html
    - cleaned up code in index.js
    - added page with content and used JSX with React

--- src/index.js
+++ src/index.js
@@ -1,17 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+//   Author:       Moya Goleski
+//   File:         index.js
+//   Last Updated: 01/16/2022
+//   Source: https://www.youtube.com/watch?v=bMknfKXIFA8
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './index.css'
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const page = (
+  <div>
+    <img src="logo512.png" width="40px"/>
+    <h1>Fun Facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on Github</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+)
+
+ReactDOM.render(page, document.getElementById("root"))
