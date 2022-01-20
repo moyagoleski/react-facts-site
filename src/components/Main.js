01cb42a174b3c83c9d74153d4e246220d85238d9
moyagoleski <moyagoleski@gmail.com>
2022-01-19 19:40:07 -0500

    Updates to Main.js and style.css
    - content is in a <main> tag instead of a <div> tag in Main.js
    - body tag got style in style.css
    - content in main section got style in style.css

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,21 +1,21 @@
 //   Author:       Moya Goleski
 //   File:         Main.js
-//   Last Updated: 01/17/2022
+//   Last Updated: 01/19/2022
 //   Source: https://www.youtube.com/watch?v=bMknfKXIFA8
 
 import React from 'react'
 
 export default function Main() {
     return (
-        <div>
-            <h1>Fun Facts about React</h1>
-                <ul>
+        <main>
+            <h1 className="main--title">Fun Facts about React</h1>
+                <ul className="main--facts">
                 <li>Was first released in 2013</li>
                 <li>Was originally created by Jordan Walke</li>
                 <li>Has well over 100k stars on Github</li>
                 <li>Is maintained by Facebook</li>
                 <li>Powers thousands of enterprise apps, including mobile apps</li>
             </ul>
-        </div>
+        </main>
     )
 }
